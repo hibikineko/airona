58f0bd2d999f922238d33e57675aae196993753c
hibikineko <abhishekshere8879@gmail.com>
2025-10-02 22:57:43 +0530

    disabled vercel

--- airona-cult/next.config.mjs
+++ airona-cult/next.config.mjs
@@ -2,18 +2,9 @@
 const nextConfig = {
     images: {
         unoptimized: true,
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "*.supabase.co",
-                pathname: "/storage/v1/object/public/**",
-            },
-            {
-                protocol: "https",
-                hostname: "cdn.discordapp.com",
-                pathname: "/**",
-            },
-        ],
+        dangerouslyAllowSVG: true,
+        contentDispositionType: 'attachment',
+        contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     },
     experimental: {
         serverActions: {
